385ae2e11f0cb3c768b887dc0159e86338f1914f
SKKUGoon <baesangil0906@gmail.com>
2021-12-28 01:02:45 +0900

    [Code Update] Add new messages.

--- settings/sysmsg.py
+++ settings/sysmsg.py
@@ -1,4 +1,11 @@
 STATUS_0 = "Database Stable"
 STATUS_1 = "Database Initiation"
+STATUS_2 = "Signal Received"
+STATUS_3 = "No Signal Received"
+STATUS_4 = "Ordering"
 
 ERROR_0 = "No New Information"
+
+
+ORDERFILL = "{}\nORDER INFO\n[{}]\nOrderID: {}\nQuantity: {}\nPrice: {}\nUsed: {}\n{}"
+ORDERFILLPADDING = "================"
