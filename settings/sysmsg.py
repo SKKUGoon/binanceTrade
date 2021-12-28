cca85b66495df945ac852959e31410b8d498805b
SKKUGoon <baesangil0906@gmail.com>
2021-12-29 02:41:49 +0900

    [Code Update] Trade Status. Order Exit Status. Order Cancel Status Message Added.

--- settings/sysmsg.py
+++ settings/sysmsg.py
@@ -2,10 +2,18 @@ STATUS_0 = "Database Stable"
 STATUS_1 = "Database Initiation"
 STATUS_2 = "Signal Received"
 STATUS_3 = "No Signal Received"
-STATUS_4 = "Ordering"
+STATUS_4 = "Ordering {} on {}"
+STATUS_5 = "Trade Fail. Order Not Filled During Window. Cancel Existing Order."
+STATUS_6 = "Trade Partial Success. Partial Order Filled. Cancel Existing Order. Waiting for Exit Window"
+STATUS_7 = "Trade Success. All Order Filled. Waiting for Exit Window"
+
+EXIT_0 = "EventDriven Trading Exit Window: Time Expired"
+EXIT_1 = "EventDriven Trading Exit Windiw: Return Fullfilled"
 
 ERROR_0 = "No New Information"
 
 
 ORDERFILL = "{}\nORDER INFO\n[{}]\nOrderID: {}\nQuantity: {}\nPrice: {}\nUsed: {}\n{}"
 ORDERFILLPADDING = "================"
+
+ORDERCANCEL = "{}\nORDER INFO\n[{}]\nOrderID: {}\nCancelled Successfully\n{}"
