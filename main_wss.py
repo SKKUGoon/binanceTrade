2a4ec6e101c6053b6b289a8390edc890b0edc325
SKKUGoon <baesangil0906@gmail.com>
2022-01-19 14:29:36 +0900

    [Code Update] <<dev>> binance websocket with spread band

--- main_wss.py
+++ main_wss.py
@@ -1,8 +1,12 @@
-from binance import ThreadedWebsocketManager, Client
+from binance import ThreadedWebsocketManager
+from binance.enums import ContractType
 from dateutil.relativedelta import FR, relativedelta
 import datetime
-import queue
+from collections import deque
 import json
+import time
+from typing import Iterable
+import numpy as np
 
 
 def get_token(target:str, typ:str, loc='key.json') -> str:
@@ -19,17 +23,81 @@ class BinanceLiveStream:
         self.pw = get_token('binance_live', 'secret_key')
 
         self.spot_close = 'init'
-        self.q_spread = queue.Queue(maxsize=20)
+        self.spld, self.plsd = deque(), deque()
+        self.spld_l, self.spld_u, self.plsd_l, self.plsd_u = (
+            None, None, None, None
+        )
 
-    def report_futr_tick(self, msg):
-        best_bid = msg['data']['b']
-        best_ask = msg['data']['a']
-        print('swap', best_bid, best_ask)
+        # Tick Spread Calc Init
+        self.deli_bid, self.deli_ask = None, None
+        self.spread_calc = False
+
+        # Band Calc Init
+        self.time = time.time()
+
+    def bollinger_band(self, obj:Iterable, apart:float=2.0) -> (float, float):
+        m = np.mean(obj)
+        s = np.std(obj)
+        return (m - apart * s, m + apart * s)
+
+    def report_perp_tick(self, msg):
+        """
+        Spread Calc
+        [SHORT TERM] - [LONG TERM]
+        (PERPETUAL)     (DELIVERY)
+        ->
+        PERP - DELI > 0
+        PERP > DELI
+        PERP SHORT, DELIVERY LONG
+        (BIDDING)     (ASKING)
+        """
+        best_bid = float(msg['data']['b'])
+        best_ask = float(msg['data']['a'])
+
+        if self.spread_calc is True:
+            # Short Perpetual Long Delivery
+            spread_spld = (best_bid - self.deli_ask) / self.deli_ask
+            # Long Perpetual Short Delivery
+            spread_plsd = (best_ask - self.deli_bid) / self.deli_bid
+
+            if self.spld_l is not None:
+                print(self.spld_l, self.spld_u, spread_spld)
+                if self.spld_l > spread_spld:
+                    print('spld signal')
+            if self.plsd_l is not None:
+                if self.plsd_u < spread_plsd:
+                    print('plsd signal')
+
+            # Queue Process
+            if len(self.spld) == 0:
+                self.spld.append(spread_spld)
+            if len(self.plsd) == 0:
+                self.plsd.append(spread_plsd)
+
+            if time.time() - self.time >= 2:
+                self.spld.append(spread_spld)
+                self.plsd.append(spread_plsd)
+
+
+                if len(self.spld) > 20:
+                    self.spld_l, self.spld_u = self.bollinger_band(self.spld)
+                    self.spld.popleft()
+
+                if len(self.plsd) > 20:
+                    self.plsd_l, self.plsd_u = self.bollinger_band(self.plsd)
+                    self.plsd.popleft()
+
+                self.time = time.time()
+
 
     def report_deli_tick(self, msg):
-        best_bid = msg['data']['b']
-        best_ask = msg['data']['a']
-        print('deli', best_bid, best_ask)
+        best_bid = float(msg['data']['b'])
+        best_ask = float(msg['data']['a'])
+
+        # Globalized variable
+        self.spread_calc = True
+        self.deli_bid = best_bid
+        self.deli_ask = best_ask
 
     def get_future_expr(self, dfmt='%Y%m%d'):
         """
@@ -59,14 +127,14 @@ class BinanceLiveStream:
         # TICKER
         expr_month = self.get_future_expr()
         twm_tick.start_symbol_ticker_futures_socket(
-            callback=self.report_futr_tick,
+            callback=self.report_perp_tick,
             symbol=symbol
-        )
+        )  # PERPETUAL
         twm_tick.start_symbol_ticker_futures_socket(
             callback=self.report_deli_tick,
             symbol=f'{symbol}{expr_month}'
-        )
-        print(f'getting deli for ETHUSDT_{expr_month}')
+        )  # CURRENT QUARTERS
+
         twm_kline.join()
         twm_tick.join()
 
