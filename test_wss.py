108e8c441ca9101cdd2e019b1e91d4a0485f8918
SKKUGoon <baesangil0906@gmail.com>
2022-02-01 01:11:57 +0900

    [Code Patch] <<dev>> checking process

--- test_wss.py
+++ test_wss.py
@@ -18,13 +18,13 @@ def get_token(target:str, typ:str, loc='key.json') -> str:
 
 
 class BinanceLiveStream:
-    def __init__(self, tick_equidistant:int=2, tick_collect:int=5):
+    def __init__(self, tick_equidistant:int=2, tick_collect:int=1):
         self.id = get_token('binance_live', 'access_key')
         self.pw = get_token('binance_live', 'secret_key')
 
         self.spot_close = 'init'
         self.spld, self.lpsd = deque(), deque()
-        self.spld_l, self.spld_u, self.plsd_l, self.plsd_u = (
+        self.spld_l, self.spld_u, self.lpsd_l, self.lpsd_u = (
             None, None, None, None
         )
         self.equidist = tick_equidistant
@@ -32,15 +32,17 @@ class BinanceLiveStream:
 
         # Tick Spread Calc Init
         self.deli_bid, self.deli_ask = None, None
+        self.prc_deli, self.prc_perp = None, None
         self.spread_calc = False
 
         # Band Calc Init
         self.time = time.time()
 
-    def bollinger_band(self, obj:Iterable, apart:float=3.0) -> (float, float):
+    @staticmethod
+    def bollinger_band(obj:Iterable, apart:float=3.5) -> (float, float):
         m = np.mean(obj)
         s = np.std(obj)
-        return (m - apart * s, m + apart * s)
+        return m - apart * s, m + apart * s
 
     def report_perp_tick(self, msg):
         """
@@ -50,39 +52,29 @@ class BinanceLiveStream:
         ->
         PERP - DELI > 0
         PERP > DELI
-        PERP SHORT, DELIVERY LONG
+        PERP SHORT, DELIVERY LONG ( SPLD )
         (BIDDING)     (ASKING)
         """
         best_bid = float(msg['data']['b'])
-        best_ask = float(msg['data']['a'])
 
         if self.spread_calc is True:
             # Short Perpetual Long Delivery
             spread_spld = (best_bid - self.deli_ask) / self.deli_ask
-            # Long Perpetual Short Delivery
-            spread_plsd = (best_ask - self.deli_bid) / self.deli_bid
 
+            print(self.prc_deli, self.prc_perp)
             if self.spld_l is not None:
-                print("perp", best_bid, best_ask)
-                print("deli", self.deli_bid, self.deli_ask)
+                # print("perp", best_bid, best_ask)
+                # print("deli", self.deli_bid, self.deli_ask)
                 if self.spld_l > spread_spld:
-                    print('spld signal')
-                    ...
-            if self.plsd_l is not None:
-                if self.plsd_u < spread_plsd:
-                    # print('lpsd signal')
-                    ...
+                    print('spld signal', 'price deli', self.prc_deli, 'price perp', self.prc_perp, 'DELI 매수1호가', self.deli_ask, 'PERP 매도1호가', best_bid)
 
             # Queue Process
             if len(self.spld) == 0:
                 self.spld.append(spread_spld)
-            if len(self.lpsd) == 0:
-                self.lpsd.append(spread_plsd)
 
             if time.time() - self.time >= self.equidist:
                 # Append spread every 2 seconds
                 self.spld.append(spread_spld)
-                self.lpsd.append(spread_plsd)
 
                 # Short Perp Long Delivery
                 if len(self.spld) > (60 / self.equidist) * self.equimin:
@@ -92,16 +84,6 @@ class BinanceLiveStream:
                     print(
                         f"Collecting data {len(self.spld)}/{(60 / self.equidist) * self.equimin}"
                     )
-
-                # Long Perp Short Delivery
-                if len(self.lpsd) > (60 / self.equidist) * self.equimin:
-                    self.plsd_l, self.plsd_u = self.bollinger_band(self.lpsd)
-                    self.lpsd.popleft()
-                else:
-                    print(
-                        f"Collecting data {len(self.lpsd)}/{(60 / self.equidist) * self.equimin}"
-                    )
-
                 self.time = time.time()
 
     def report_deli_tick(self, msg):
@@ -113,7 +95,8 @@ class BinanceLiveStream:
         self.deli_bid = best_bid
         self.deli_ask = best_ask
 
-    def get_future_expr(self, dfmt='%Y%m%d'):
+    @staticmethod
+    def get_future_expr(dfmt='%Y%m%d'):
         """
         Binance Future always expires on
         LAST FRIDAY OF THE MONTH.
@@ -130,6 +113,12 @@ class BinanceLiveStream:
                 relativedelta(day=31, weekday=FR(-1), months=expr_month[m])
         )
         return f"_{last_fri.strftime(dfmt)[2:]}"
+
+    def get_deli_kline(self, msg):
+        self.prc_deli = float(msg['k']['c'])
+
+    def get_perp_kline(self, msg):
+        self.prc_perp = float(msg['k']['c'])
 
     def main(self, symbol:str):
         twm_kline = ThreadedWebsocketManager(self.id, self.pw)
@@ -148,6 +137,18 @@ class BinanceLiveStream:
             callback=self.report_deli_tick,
             symbol=f'{symbol}{expr_month}'
         )  # CURRENT QUARTERS
+
+        # Price Checker
+        twm_kline.start_kline_futures_socket(
+            callback=self.get_perp_kline,
+            symbol=f'{symbol}'
+        )
+        twm_kline.start_kline_futures_socket(
+            callback=self.get_deli_kline,
+            symbol=symbol,
+            contract_type=ContractType.CURRENT_QUARTER
+        )
+        print(f'{symbol}{expr_month}')
 
         twm_kline.join()
         twm_tick.join()
