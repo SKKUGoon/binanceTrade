5da4f2f34e352ae5477be4b8deb270cf8a99f397
SKKUGoon <baesangil0906@gmail.com>
2022-02-06 15:54:08 +0900

    [Code Update] <<dev>> multiprocess tools

---
+++ main.py
@@ -0,0 +1,20 @@
+import broadcast
+from socket_client import clientb00
+from socket_client import clientb01
+from socket_client import clientb02
+from socket_client import clientm00
+from socket_client import clientm01
+from socket_client import clientm02
+from socket_client import clientm03
+from socket_client import clientf00
+from socket_client import clientf01s
+from socket_client import clientf01l
+
+
+from multiprocessing import Process
+import os
+
+
+
+
+broadcast.broadcast()
