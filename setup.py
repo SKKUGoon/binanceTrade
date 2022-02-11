f33879c8ea971df5c076fe812b807c10623b6c4d
SKKUGoon <baesangil0906@gmail.com>
2022-02-11 23:27:41 +0900

    [Code Update] <<setup>> setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+# python setup.py develop
+setup(
+    name='binanceTrade',
+    version='0.3a',
+    packages=['dbms', 'model', 'settings', 'socket_client'],
+    url='',
+    license='',
+    author='Sang Il, Bae',
+    author_email='baesangil0906@gmail.com',
+    description='binance trading program with websockets'
+)
