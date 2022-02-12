0ccf976671ae0a1f36e278f720b864fad85f6c4b
SKKUGoon <baesangil0906@gmail.com>
2022-02-13 03:37:13 +0900

    [Code Update] <<discord>> discord chatbot for on-request report

--- socket_client/clientb03.py
+++ socket_client/clientb03.py
@@ -1,8 +1,9 @@
+from dbms.Ddbms import LocalDBMethods
+from socket_client import clienttree
 from settings import sysmsg as sysmsgs
 from settings import _global_ as const
 from settings import color as bc
 from settings import table
-from dbms.Ddbms import LocalDBMethods
 
 from discord.ext import commands
 import discord
@@ -22,13 +23,7 @@ import os
 # Get Messages and Record it in Database
 # FREQ: On request
 
-
-PREFIX = "!"
-BOT = commands.Bot(command_prefix=PREFIX)
-server = LocalDBMethods()
-
-
-def get_token(target:str, typ:str, loc='..\key.json') -> str:
+def get_token(target: str, typ: str, loc='..\key.json') -> str:
     if platform.system() == 'Windows':
         pass
     else:
@@ -39,45 +34,56 @@ def get_token(target:str, typ:str, loc='..\key.json') -> str:
     return dat[target][typ]
 
 
-async def on_ready():
-    game = discord.Game("Booted Up")
-    await BOT.change_presence(
-        status=discord.Status.online,
-        activity=game
-    )
-    print(sysmsgs.BACK03_DISCORD_READY)
+def db_addr():
+    if platform.system() == 'Windows':
+        return '../TDB.db'
+    else:
+        return '/home/goon/crypto/binanceTrade/TDB.db'
 
 
-async def on_message(message):
-    if message.author.BOT:
-        return None
+class CryptoChat(discord.Client):
+    PREFIX = "!"
+    BOT = commands.Bot(command_prefix=PREFIX)
+    server = LocalDBMethods(db_addr())
+    server.set_wal()
 
-    await BOT.process_commands(message)
-
-
-@BOT.command(name='connection')
-async def connection_check(ctx):
-    # await ctx.channel.send("")
-    print(ctx.command)
-    dt = datetime.now().strftime('D%Y%m%dT%H:%M:%S')
-
-
-@BOT.command(name='restart')
-async def connection_restart(ctx):
-    print(ctx.command)
+    async def on_ready(self):
+        game = discord.Game("Booted Up")
+        await self.BOT.change_presence(
+            status=discord.Status.online,
+            activity=game
+        )
+        print(sysmsgs.BACK03_DISCORD_READY)
 
 
-@BOT.command(name='trade')
-async def report_trade(ctx):
-    print(ctx.command)
+    async def on_message(self, message):
+        if message.author.BOT:
+            return None
+
+        await self.BOT.process_commands(message)
+
+    @staticmethod
+    @BOT.command(name='connection')
+    async def connection_check(ctx):
+        # await ctx.channel.send("")
+        print(ctx.command)
 
 
-async def test(ctx):
-    print('ping')
+    @staticmethod
+    @BOT.command(name='restart')
+    async def connection_restart(ctx):
+        print(ctx.command)
+
+    @staticmethod
+    @BOT.command(name='trade')
+    async def report_trade(ctx):
+        print(ctx.command)
+
+    @staticmethod
+    async def test(ctx):
+        print('ping')
 
 
 if __name__ == "__main__":
-    # chat = TradeReport()
-    # chat.BOT.run(get_token('discord', 'bot_token'))
-    # asyncio.get_event_loop().run_until_complete(listen())
-    BOT.run(get_token('discord', 'bot_token'))
+    chat = CryptoChat()
+    chat.BOT.run(get_token('discord', 'bot_token'))
---
+++ socket_client/clienttree.py
@@ -0,0 +1,12 @@
+# GLOBAL IMMUTABLE VARIABLE
+MODULES = {
+    "middle": {
+        "m00": "Upbit News Watcher",
+        "m01": "BitThumb News Watcher",
+        "m02": "Spread Watcher",
+    },
+    "front": {
+        "f00": "Spot Trader",
+        "f01": "Spread Trader"
+    },
+}
